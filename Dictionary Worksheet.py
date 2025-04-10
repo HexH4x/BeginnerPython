1bbbe7787f8077c68fc187b0cd7697512af63f07
HexH4x <la.lindsey.armstrong@gmail.com>
2025-04-10 15:21:05 -0500

    Create Dictionary Worksheet.py

---
+++ Dictionary Worksheet.py
@@ -0,0 +1,88 @@
+# Create a dictionary of a person
+person = {
+    'name': 'Hex',
+    'age': 102,
+    'city': 'Kings Landing'
+}
+print("Original dictionary:", person)
+
+# Access values by key
+print("Name:", person['name'])
+print("Name", person.get('name'))
+print("City:", person.get('city'))
+print("City", person['city'])
+
+# Add a new key-value pair
+person['job'] = 'Mercenary'
+print("After adding job:", person)
+
+# Change a value
+person['age'] = 106
+print("After updating age:", person)
+
+# Remove a key-value pair
+del person['city']
+print("After deleting city:", person)
+
+# Loop through keys and values
+for key, value in person.items():
+    print(f"{key} => {value}")
+
+# Get list of keys and values
+print("Keys:", list(person.keys()))
+print("Values:", list(person.values()))
+
+
+# Create a dictionary
+___ = {
+    '___': '___',
+    '___': '___'
+}
+print("Original dictionary:", ___)
+
+# Access values by key
+print("___:", ___['___'])
+print("___:", ___.get('___'))
+
+# Add a new key-value pair
+___['___'] = '___'
+print("After adding new pair:", ___)
+
+# Change a value
+___['___'] = "___"
+print("After changing value:", ___)
+
+# Remove a key-value pair
+del ___['___']
+print("After deleting:", ___)
+
+# Loop through keys and values
+for key, value in ___.items():
+    print(f"{key} => {value}")
+
+# Get a list of keys and values
+print("Keys:", list(___.keys()))
+print("Values:", list(___.values()))
+
+
+
+# New
+# Create a new dictionary
+
+
+# Access values by key
+
+
+# Add a new key-value pair
+
+
+# Change a value
+
+
+# Remove a key-value pair
+
+
+# Loop through keys and values
+
+
+# Get list of keys and values
